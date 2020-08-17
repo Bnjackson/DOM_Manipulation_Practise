fe5529ae6f42476a4368c8bce5f2d1f502132b82
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-08-17 02:06:59 +0100

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+//DOM - Document Object Model - The DOM is a structured representation of an HTML document. The DOM is used to connect webpages to scripts like JavaScript. JavaScript and the DOM are two different things.
+//When a webpage loads the browser creates a Document Object Model of the page. The HTML DOM is constructed as a tree of objects.
+//With the Object Model JavaScript can change, add new, remove and react to HTML elements and CSS styles. 
